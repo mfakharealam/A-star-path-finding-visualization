a2d6ed1ea54fa0c1df2a774b62b2811acc28010c
MFA571 <fakhar571@gmail.com>
2019-07-17 23:16:18 +0500

    added grid and node files
    detects collision

--- Pathfinding/Assets/Grid.cs
+++ Pathfinding/Assets/Grid.cs
@@ -4,13 +4,49 @@ using UnityEngine;
 
 public class Grid : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public Vector2 gridWorldSize;
+    public float nodeRadius;
+    public LayerMask unwalkableMask;
+    Node[,] grid;
+    float nodeDiameter;
+    int gridSizeX, gridSizeY;
+
+
+    private void Start()
+    {
+        nodeDiameter = nodeRadius * 2;
+        gridSizeX = Mathf.RoundToInt(gridWorldSize.x / nodeDiameter);
+        gridSizeY = Mathf.RoundToInt(gridWorldSize.y / nodeDiameter);
+        CreateGrid();
+
+    }
+    private void CreateGrid()
+    {
+        grid = new Node[gridSizeX, gridSizeY];
+        Vector3 worldBottomLeft = transform.position - Vector3.right * gridWorldSize.x / 2 - Vector3.forward * gridWorldSize.y / 2;
+        for (int x = 0; x < gridSizeX; x++)
+        {
+            for (int y = 0; y < gridSizeY; y++)
+            {
+                Vector3 worldPoint = worldBottomLeft + Vector3.right * (x * nodeDiameter + nodeRadius) + Vector3.forward * (y * nodeDiameter + nodeRadius);
+                bool walkable = !(Physics.CheckSphere(worldPoint, nodeRadius, unwalkableMask));
+                grid[x, y] = new Node(walkable, worldPoint);
+
+            }
+
+        }
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(transform.position, new Vector3(gridWorldSize.x, 1, gridWorldSize.y));
+        if (grid != null)
+        {
+            foreach (Node n in grid)
+            {
+                Gizmos.color = (n.walkable) ? Color.white : Color.red;
+                Gizmos.DrawCube(n.worldPosition, Vector3.one * (nodeDiameter - .1f));
+            }
+        }
+    }
+
 }
--- Pathfinding/Assets/Node.cs
+++ Pathfinding/Assets/Node.cs
@@ -2,15 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Node : MonoBehaviour {
+public class Node {
+    public bool walkable;
+    public Vector3 worldPosition;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public Node(bool _walkable, Vector3 _worldPosition)
+    {
+        walkable = _walkable;
+        worldPosition = _worldPosition;
+    }
+                                        
 }
